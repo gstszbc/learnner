19ebe13dfc91b302c3198a658071bc96f4667dd0
zhangbicheng <zhangbicheng@tencent.com>
2022-07-12 09:09:04 +0800

    Initial commit

---
+++ src/main/java/com/example/learnner/LearnnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnner/LearnnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
